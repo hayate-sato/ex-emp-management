9cfc80b44a926ed855801d913643ca50621c16e3
hayate-sato <hayate.sato@rakus-partners.co.jp>
2022-05-16 11:56:08 +0900

    入力値チェック処理完了

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.sample.domain.Employee;
 import jp.co.sample.form.UpdateEmployeeForm;
@@ -43,7 +46,11 @@ public class EmployeeController {
 	}
 
 	@RequestMapping("/update")
-	public String update(UpdateEmployeeForm form) {
+	public String update(@Validated UpdateEmployeeForm form, BindingResult result,
+			RedirectAttributes redirectAttributes, Model model) {
+		if (result.hasErrors()) {
+			return showDetail(form.getId(), model);
+		}
 
 		int idInt = Integer.parseInt(form.getId());
 		Employee employee = employeeServise.showDetail(idInt);
